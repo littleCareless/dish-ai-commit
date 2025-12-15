3633f64765aa5ee1d34feffbc7923f0957139095
zhangning <7148505@qq.com>
2025-12-15 15:53:37 +0800

    🔧 chore(deps): 优化依赖管理以支持项目重构目标
    - 移除根目录 `package.json` 中的依赖项 `@simonorzel26/ai-models`
    - 更新 `src/package.json`：
      - 移除依赖项 `@dish-ai-commit/webview-ui`
      - 添加依赖项 `zod`，支持数据验证
    - 【优化】简化依赖解析和构建流程，符合新的项目结构重构目标

---
+++ src/commitlint.config.mjs
@@ -0,0 +1,38 @@
+export default {
+  extends: ["gitmoji"],
+  rules: {
+    "header-max-length": [2, "always", 108],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "init",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "revert",
+        "chore",
+        "build",
+        "ci",
+
+        "✨ feat",
+        "🐛 fix",
+        "🎉 init",
+        "📚 docs",
+        "🎨 style",
+        "♻️ refactor",
+        "⚡ perf",
+        "✅ test",
+        "⏪ revert",
+        "🔧 chore",
+        "🔨 build",
+        "⚙️ ci",
+      ],
+    ],
+  },
+};
